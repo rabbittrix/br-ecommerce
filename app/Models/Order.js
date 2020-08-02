e360287570f8ac3cdf4ff96aa02578c2f9ba8b81
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-02 20:14:09 +0100

    OrderHook create

--- app/Models/Order.js
+++ app/Models/Order.js
@@ -1,9 +1,17 @@
 'use strict'
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+
 const Model = use('Model')
 
 class Order extends Model {
+
+  static boot(){
+    super.boot()
+    this.addHook('afterFind', 'OrderHook.updateValues')
+    this.addHook('afterPaginate', 'OrderHook.updateCollectionValues')
+  }
+
   items(){
     return this.hasMany('App/Models/OrderItem')
   }
