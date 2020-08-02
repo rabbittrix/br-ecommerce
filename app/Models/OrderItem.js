6549f9a326d24aa5cdeae07ad5aaf983a147a853
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-02 20:21:57 +0100

    OrderItemHook created

--- app/Models/OrderItem.js
+++ app/Models/OrderItem.js
@@ -4,6 +4,13 @@
 const Model = use('Model')
 
 class OrderItem extends Model {
+
+  static boot(){
+    super.boot()
+    this.addHook('beforeSave', 'OrderItemHook.updateSubtotal')
+    //this.addHook('afterPaginate', 'OrderHook.updateCollectionValues')
+  }
+
   static get traits(){
     return ['App/Models/Traits/NoTimestamp']
   }
