0c12090039e88c060dfe4acbb8f4368bb5001dfe
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-04 21:45:41 +0100

    Implementation UserContrller

--- app/Controllers/Http/Admin/UserController.js
+++ app/Controllers/Http/Admin/UserController.js
@@ -4,6 +4,8 @@
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
+const User = use('App/Models/User')
+
 /**
  * Resourceful controller for interacting with users
  */
@@ -16,20 +18,18 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    * @param {View} ctx.view
+   * @param {object} ctx.pagination
    */
-  async index ({ request, response, view }) {
-  }
-
-  /**
-   * Render a form to be used for creating a new user.
-   * GET users/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
+  async index ({ request, response, pagination }) {
+    const name = request.input('name')
+    const query = User.query()
+    if(name){
+      query.where('name', 'LIKE', `%${name}%`)
+      query.orWhere('surname', 'LIKE', `%${name}%`)
+      query.orWhere('email', 'LIKE', `%${name}%`)
+    }
+    const users = await query.paginate(pagination.page, pagination.limit)
+    return response.send(users)
   }
 
   /**
@@ -41,6 +41,15 @@ class UserController {
    * @param {Response} ctx.response
    */
   async store ({ request, response }) {
+    try {
+      const userData = request.only(['name', 'surname', 'email', 'password', 'image_id'])
+      const user = await User.create(userData)
+      return response.status(201).send(user)
+    } catch (error) {
+      return response.status(400).send({
+        message: 'Error created user!'
+      })
+    }
   }
 
   /**
@@ -52,19 +61,9 @@ class UserController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async show ({ params, request, response, view }) {
-  }
-
-  /**
-   * Render a form to update an existing user.
-   * GET users/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
+  async show ({ params: {id}, response}) {
+    const user = await User.findOrFail(id)
+    return response.send(user)
   }
 
   /**
@@ -75,7 +74,12 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async update ({ params, request, response }) {
+  async update ({ params: {id}, request, response }) {
+    const user = await User.findOrFail(id)
+    const userData = request.only(['name', 'surname', 'email', 'password', 'image_id'])
+    user.merge(userData)
+    await user.save()
+    return response.send(user)
   }
 
   /**
@@ -86,7 +90,18 @@ class UserController {
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async destroy ({ params, request, response }) {
+  async destroy ({ params: {id}, request, response }) {
+    const user = await User.findOrFail(id)
+    try {
+      await user.delete()
+      return response.status(204).send({
+        message: 'Registration deleted successfully.'
+      })
+    } catch (error) {
+      return response.status(500).send({
+        message: 'Registration deleted fail.'
+      })
+    }
   }
 }
 
