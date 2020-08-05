9d577a283153f9499d8e6a7f5ab9acdbef6f6a2a
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-05 18:59:47 +0100

    middleware auth

--- start/routes/admin.js
+++ start/routes/admin.js
@@ -11,9 +11,9 @@ Route.group(() => {
   Route.post('orders/:id/discount', 'OrderController.applyDiscount')
   Route.delete('orders/:id/discount', 'OrderController.removeDiscount')
   Route.resource('orders', 'OrderController').apiOnly()
-  
+
   Route.resource('images', 'ImageController').apiOnly()
   Route.resource('users', 'UserController').apiOnly()
 
 
-}).prefix('v1/admin').namespace('Admin')
+}).prefix('v1/admin').namespace('Admin').middleware(['auth', 'is:(admin || manager'])
