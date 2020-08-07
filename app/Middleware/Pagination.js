e4eb05f4d760a9862ed1e7b71811140f165156cc
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-07 16:53:33 +0100

    fix bug

--- app/Middleware/Pagination.js
+++ app/Middleware/Pagination.js
@@ -9,20 +9,23 @@ class Pagination {
    * @param {Request} ctx.request
    * @param {Function} next
    */
-  async handle ({ ctx }, next) {
+  async handle(ctx, next) {
     // call next to advance the request
-    if(ctx.request.method() === 'GET'){
-      const page = parseInt(ctx.request.input('page'))
-      const limit = parseInt(ctx.request.input('limit'))
-
+    if (ctx.request.method() === 'GET') {
+      const page = ctx.request.input('page')
+        ? parseInt(ctx.request.input('page'))
+        : 1
+      const limit = ctx.request.input('limit')
+        ? parseInt(ctx.request.input('limit'))
+        : 20
       ctx.pagination = {
         page,
         limit
       }
 
       const perpage = parseInt(ctx.request.input('perpage'))
-      if(perpage){
-        ctx.Pagination.limit = perpage
+      if (perpage) {
+        ctx.pagination.limit = perpage
       }
     }
     await next()
