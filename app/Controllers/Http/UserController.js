ac5352b65c697d19e16a56132fb964b165b8da31
Roberto de Souza <rabbittrix@hotmail.com>
2020-08-07 16:50:57 +0100

    adjusts

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,14 +1,11 @@
 'use strict'
 
 const UserTransformer = use('App/Transformers/Admin/UserTransformer')
-
 class UserController {
-  async me({response, transform, auth}){
+  async me({ response, transform, auth }) {
     var user = await auth.getUser()
     const userData = await transform.item(user, UserTransformer)
     userData.roles = await user.getRoles()
     return response.send(userData)
   }
 }
-
-module.exports = UserController
